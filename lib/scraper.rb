3f5f97373ccf1f797e40b288dadece44fc9a9980
Vesna Vucinic <vesna.vucinic68@gmail.com>
2019-11-15 10:35:11 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,12 +10,12 @@ class Scraper
     Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
   end
  
- #The #get_courses instance method will be responsible for using a CSS selector to grab all of the HTML elements that contain a course. In other words, the return value of this method should be a collection of Nokogiri XML elements, each of which describes a course offering. We're going to have to examine the page with the element inspector to find the CSS selector that contains the courses.
+     #The #get_courses instance method will be responsible for using a CSS selector to grab all of the HTML elements that contain a course. In other words, the return value of this method should be a collection of Nokogiri XML elements, each of which describes a course offering. We're going to have to examine the page with the element inspector to find the CSS selector that contains the courses.
   def get_courses
     self.get_page.css(".post")
   end
  
- #The #make_courses method will be responsible for actually instantiating Course objects and giving each course object the correct title, schedule and description attribute that we scraped from the page.
+     #The #make_courses method will be responsible for actually instantiating Course objects and giving each course object the correct title, schedule and description attribute that we scraped from the page.
   def make_courses
     self.get_courses.each do |post|
       course = Course.new
@@ -25,7 +25,7 @@ class Scraper
     end
   end
  
- #calls on .make_courses and then iterates over all of the courses that get created to puts out a list of course offerings
+     #calls on .make_courses and then iterates over all of the courses that get created to puts out a list of course offerings
   def print_courses
     self.make_courses
     Course.all.each do |course|
